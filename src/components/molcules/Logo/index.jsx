9cf015d5f494edf9cab7e1e92165e45953e98eae
bbumjun <bumjun7928@gmail.com>
2021-05-25 16:33:00 +0900

    ✨ add Logo Component

---
+++ src/components/molcules/Logo/index.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import * as S from './style';
+
+const Logo = () => <S.Logo fontSize={2}>LOGO</S.Logo>;
+
+export default Logo;
---
+++ src/components/molcules/Logo/style.js
@@ -0,0 +1,3 @@
+import styled from 'styled-components';
+import Text from 'components/atoms/Text';
+export const Logo = styled(Text)``;
