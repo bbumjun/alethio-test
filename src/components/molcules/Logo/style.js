100233113d7636b90a64b6518e7d32cc66db213c
bbumjun <bumjun7928@gmail.com>
2021-05-25 18:06:58 +0900

    ✨ add Logo Component

--- src/components/molcules/Logo/style.js
+++ src/components/molcules/Logo/style.js
@@ -1,3 +1,6 @@
 import styled from 'styled-components';
-import Text from 'components/atoms/Text';
-export const Logo = styled(Text)``;
+import { Link } from 'components/atoms';
+export const Logo = styled(Link)`
+  font-size: 2rem;
+  font-weight: bold;
+`;
