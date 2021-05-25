1dbaf85a6eb64bb44e5621d86f440ee2f3e01535
bbumjun <bumjun7928@gmail.com>
2021-05-25 22:58:53 +0900

    ♻️ add type prop

--- src/components/atoms/Button/index.jsx
+++ src/components/atoms/Button/index.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import * as S from './style';
-const Button = ({ handleClick, children, className }) => {
+const Button = ({ handleClick, type, children, className }) => {
   return (
-    <S.Button onClick={handleClick} className={className}>
+    <S.Button type={type} onClick={handleClick} className={className}>
       {children}
     </S.Button>
   );
