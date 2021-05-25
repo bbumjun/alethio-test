4ffba3281d940c009aa1a2c7c4f9edccfd217046
bbumjun <bumjun7928@gmail.com>
2021-05-25 18:07:41 +0900

    Add NavBar Component

---
+++ src/components/molcules/NavBar/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import * as S from './style';
+
+const NavBar = () => {
+  return (
+    <S.Wrapper>
+      <S.HorizontalNavList />
+    </S.Wrapper>
+  );
+};
+
+export default NavBar;
---
+++ src/components/molcules/NavBar/style.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+// import { NavList } from 'components/molcules';
+import NavList from 'components/molcules/NavList';
+export const Wrapper = styled.nav`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  @media screen and (max-width: ${(props) => props.theme.deviceSizes.mobile}) {
+    display: none;
+  }
+`;
+
+export const HorizontalNavList = styled(NavList)`
+  width: 15rem;
+  display: flex;
+  justify-content: space-between;
+`;
