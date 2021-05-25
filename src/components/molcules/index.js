b874ebcd3803aac75860937a201ed6da56bb2909
bbumjun <bumjun7928@gmail.com>
2021-05-25 20:17:57 +0900

    🎨 add mocules index file

---
+++ src/components/molcules/index.js
@@ -0,0 +1,5 @@
+export { default as Logo } from './Logo';
+export { default as NavBar } from './NavBar';
+export { default as NavItem } from './NavItem';
+export { default as NavList } from './NavList';
+export { default as NavView } from './NavView';
