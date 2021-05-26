5e6924d0ee0d4eb83ee37a62a1423dddfab8837b
bbumjun <bumjun7928@gmail.com>
2021-05-26 18:32:59 +0900

    💄 update button basic style

--- src/components/atoms/Button/style.js
+++ src/components/atoms/Button/style.js
@@ -1,7 +1,11 @@
 import styled from 'styled-components';
 
 export const Button = styled.button`
-  padding: 0;
-  border: none;
+  padding: 0.5rem;
+  border: solid black 0.1rem;
+  border-radius: 5px;
   background: none;
+  &:hover {
+    cursor: pointer;
+  }
 `;
