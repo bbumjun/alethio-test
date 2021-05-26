81a2efc526eaf7d4cb12e081c112f0945f88cad8
bbumjun <bumjun7928@gmail.com>
2021-05-26 20:05:30 +0900

    🔥 remove unnecessary code

--- src/components/pages/SignUp/style.js
+++ src/components/pages/SignUp/style.js
@@ -1,7 +1,6 @@
 import styled from 'styled-components';
 
 export const Container = styled.section`
-  /* max-width: ${(props) => props.theme.screen.medium}; */
   display: flex;
   flex-direction: column;
   justify-content: center;
