30e9a103006dbcf687ea33b37a1d7d2b0ed9ddad
bbumjun <bumjun7928@gmail.com>
2021-05-26 18:35:06 +0900

    💄 update basic form input style

--- src/components/molcules/FormInput/style.js
+++ src/components/molcules/FormInput/style.js
@@ -4,4 +4,8 @@ import styled from 'styled-components';
 export const Label = styled.label`
   margin: 0.5rem;
 `;
-export const StyledInput = styled(Input)``;
+export const StyledInput = styled(Input)`
+  font-size: 1.2rem;
+  border-radius: 5px;
+  padding: 0.5rem 0.1rem 0 0.1rem;
+`;
