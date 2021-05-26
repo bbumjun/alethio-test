d2abc638a6658333ec756d1e14165542258ee27b
bbumjun <bumjun7928@gmail.com>
2021-05-27 02:21:18 +0900

    ♻️ fix login method

--- src/components/molcules/NavList/index.jsx
+++ src/components/molcules/NavList/index.jsx
@@ -4,20 +4,22 @@ import { NavItem } from 'components/molcules';
 import { useRecoilValue, useSetRecoilState } from 'recoil';
 import auth from 'recoil/auth';
 import { message } from 'common/config';
-
+import { useHistory } from 'react-router-dom';
 const NavList = ({ className, handleClick }) => {
   const authenticated = useRecoilValue(auth.authenticatedState);
   const setToken = useSetRecoilState(auth.tokenState);
+  const history = useHistory();
   const logout = () => {
     setToken(null);
     alert(message.success.LOGOUT);
+    history.push('/');
   };
   return (
     <S.NavList className={className} onClick={handleClick}>
       {authenticated ? (
         <>
           <NavItem url="/mypage-order">마이페이지</NavItem>
-          <NavItem url="/" onClick={logout}>
+          <NavItem url="/logout" onClick={logout}>
             로그아웃
           </NavItem>
         </>
