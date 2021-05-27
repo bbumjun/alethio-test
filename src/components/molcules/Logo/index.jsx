332628f44e4dac6f7af6e8d424fc1fd2882e969c
bbumjun <bumjun7928@gmail.com>
2021-05-28 02:07:25 +0900

    💄 update logo name

--- src/components/molcules/Logo/index.jsx
+++ src/components/molcules/Logo/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import * as S from './style';
 
-const Logo = () => <S.Logo to="/">LOGO</S.Logo>;
+const Logo = () => <S.Logo to="/">ALETHIO</S.Logo>;
 
 export default Logo;
--- src/components/molcules/Logo/style.js
+++ src/components/molcules/Logo/style.js
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 import { Link } from 'components/atoms';
 export const Logo = styled(Link)`
-  font-size: 2rem;
+  font-size: 2.5rem;
   font-weight: bold;
 `;
