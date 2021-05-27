ab466e24526caa5f453cd7de8648be5e40c9b1a1
bbumjun <bumjun7928@gmail.com>
2021-05-27 19:19:14 +0900

    🎨update index file

--- src/components/atoms/index.js
+++ src/components/atoms/index.js
@@ -3,3 +3,6 @@ export { default as Button } from './Button';
 export { default as Link } from './Link';
 export { default as Image } from './Image';
 export { default as Input } from './Input';
+export { default as List } from './List';
+export { default as ListItem } from './ListItem';
+export { default as Label } from './Label';
