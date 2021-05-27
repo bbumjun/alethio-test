95316f8949f4c02cbf8598172f9c8803412c993a
bbumjun <bumjun7928@gmail.com>
2021-05-28 02:09:36 +0900

    🎨move recolroot t o wrap app component

--- src/App.js
+++ src/App.js
@@ -2,29 +2,36 @@ import Header from 'components/organizms/Header';
 import React from 'react';
 import { ThemeProvider } from 'styled-components';
 import theme from 'common/theme';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import { RecoilRoot } from 'recoil';
-import { SignUp, Login, Home } from 'components/pages';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from 'react-router-dom';
+import { SignUp, Login, Home, MyPage } from 'components/pages';
+import { useRecoilValue } from 'recoil';
+import auth from 'recoil/auth';
 function App() {
+  const isAuthenticated = useRecoilValue(auth.authenticatedState);
   return (
     <Router>
       <ThemeProvider theme={theme}>
-        <RecoilRoot>
-          <Header />
-          <Switch>
-            <Route exact path="/">
-              <Home />
-            </Route>
-            <Route path="/login">
-              <Login />
-            </Route>
-            <Route path="/logout"></Route>
-            <Route path="/sign-up">
-              <SignUp />
-            </Route>
-            <Route path="/mypage/order"></Route>
-          </Switch>
-        </RecoilRoot>
+        <Header />
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/logout"></Route>
+          <Route path="/sign-up">
+            <SignUp />
+          </Route>
+          <Route path="/mypage/order">
+            {isAuthenticated ? <MyPage /> : <Redirect to="/" />}
+          </Route>
+        </Switch>
       </ThemeProvider>
     </Router>
   );
