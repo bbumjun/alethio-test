e3090b6a0272cfc8546aed9f3cc40e40c77b5dd6
bbumjun <bumjun7928@gmail.com>
2021-05-27 19:17:15 +0900

    💄 add List component

---
+++ src/components/atoms/List/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const StyledList = styled.ul`
+  padding: 0;
+  margin: 0;
+`;
+
+const List = ({ children, className }) => {
+  return <StyledList className={className}>{children}</StyledList>;
+};
+
+export default List;
