0472eb034b470cb57e1413c2907014c1e2aa1a29
bbumjun <bumjun7928@gmail.com>
2021-05-29 00:39:07 +0900

    ✨ atomEffect 사용해 token을 로컬스토리지에 저장

--- src/recoil/auth.js
+++ src/recoil/auth.js
@@ -1,8 +1,26 @@
-import { atom, selector } from 'recoil';
+import { atom, selector, DefaultValue } from 'recoil';
+
+const localStorageEffect =
+  (key) =>
+  ({ setSelf, onSet }) => {
+    const savedValue = localStorage.getItem(key);
+    if (savedValue != null) {
+      setSelf(JSON.parse(savedValue));
+    }
+
+    onSet((newValue) => {
+      if (newValue instanceof DefaultValue) {
+        localStorage.removeItem(key);
+      } else {
+        localStorage.setItem(key, JSON.stringify(newValue));
+      }
+    });
+  };
 
 const tokenState = atom({
   key: 'tokenState',
   default: null,
+  effects_UNSTABLE: [localStorageEffect('token')],
 });
 
 const authenticatedState = selector({
