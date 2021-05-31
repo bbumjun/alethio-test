46c9c7297c4829984438a24582507a7ce3a35213
bbumjun <bumjun7928@gmail.com>
2021-05-31 21:24:29 +0900

    🎨 alert 메시지 도메인에 따라 분리

--- src/common/constants.js
+++ src/common/constants.js
@@ -3,38 +3,48 @@ import axios from 'axios';
 axios.defaults.baseURL = 'http://106.10.53.116:8099';
 axios.defaults.headers.post['Content-Type'] = 'application/json';
 
-export const message = {
-  error: {
-    ERROR_OCCURRED: '문제가 발생했습니다.',
-    ERROR_BY_CLIENT: '잘못된 요청입니다.',
-    ERROR_BY_SERVER: '서버로부터 응답을 받지 못했습니다.',
-    DIFFERENT_PASSWORD: '비밀번호가 일치하지 않습니다.',
+export const messages = {
+  SIGNUP: {
+    SUCCESS: '회원가입이 완료되었습니다.',
+    FAILURE: '회원가입이 실패했습니다.',
   },
-  success: {
-    SIGNUP: '회원가입에 성공했습니다.',
-    LOGOUT: '로그아웃 되었습니다.',
-    ORDER: '주문이 완료되었습니다.',
+  LOGIN: {
+    SUCCESS: '로그인 되었습니다.',
+    FAILURE: '로그인에 실패했습니다.',
+    REQUIRE: '로그인이 필요합니다.',
   },
-  confirm: {
-    EMAIL: '이메일을 다시 확인해주세요.',
-    PASSWORD: '비밀번호를 다시 확인해주세요.',
-    MOBILE: '전화번호를 다시 확인해주세요.',
+
+  LOGOUT: {
+    SUCCESS: '로그아웃 되었습니다.',
+    FAILURE: '로그아웃이 실패했습니다.',
   },
-  type: {
-    EMAIL: '이메일을 입력해주세요.',
-    PASSWORD: '비밀번호를 입력해주세요.',
-    PASSWORD_AGAIN: '비밀번호 확인',
-    MOBILE: '전화번호를 입력해주세요.',
+  ORDER: {
+    CONFIRM: '주문하시겠습니까?',
+    SUCCESS: '주문이 완료되었습니다.',
+    FAILURE: '주문에 실패했습니다.',
   },
-  failure: {
-    LOGIN: '로그인에 실패했습니다.',
-    SIGNUP: '회원가입에 실패헀습니다.',
-    FETCH: '데이터를 불러오는데 실패했습니다.',
+  EMAIL: {
+    HINT: '이메일을 입력해주세요.',
+    CONFIRM: '이메일을 다시 확인해주세요.',
   },
-  require: {
-    LOGIN: '로그인이 필요합니다.',
+  PASSWORD: {
+    HINT: '비밀번호를 입력해주세요.',
+    CONFIRM: '비밀번호를 다시 확인해주세요.',
+    DISCORDANCE: '비밀번호가 불일치합니다.',
   },
-  question: {
-    ORDER: '이 제품을 주문하시겠습니까?',
+  PASSWORD_AGAIN: {
+    HINT: '입력한 비밀번호와 같게 입력해주세요.',
+  },
+  MOBILE: {
+    HINT: '전화번호를 입력해주세요.',
+    CONFIRM: '전화번호를 다시 확인해주세요.',
+  },
+  ERROR: {
+    BAD_REQUEST: '잘못된 요청입니다.',
+    UNAUTHORIZED: '로그인이 필요합니다.',
+    FORBIDDEN: '접근할 수 있는 권한이 없습니다.',
+    NOT_FOUND: '찾을 수 없는 페이지입니다.',
+    INTERNAL_SERVER_ERROR: '서버에 문제가 있어 요청을 수행할 수 없습니다.',
+    SOMETHING_WRONG: '오류가 발생했습니다.',
   },
 };
